a78bdd33a083c9300e8e6b1fdc46d6a4d85253d3
Adrian Stefan <92085719+AdrianST19@users.noreply.github.com>
2021-11-17 12:28:40 +0200

    Added a file

--- TicTacToe.java
+++ TicTacToe.java
@@ -8,20 +8,20 @@ public class TicTacToe {
 		Scanner scanner = new Scanner(System.in);
 		
 		char[][] board = {{' ', ' ', ' '},
-				 	      {' ', ' ', ' '}, 
-				 	      {' ', ' ', ' '}};
+				  {' ', ' ', ' '}, 
+				  {' ', ' ', ' '}};
 		
 		printBoard(board);
 		
 		while (true) {
 			playerTurn(board, scanner);
-			if (isGameFinished(board)){
+			if (isGameFinished(board)){ // it manages to get out of this while loop if the player wins
 				break;
 			}
 			printBoard(board);
 			
 			computerTurn(board);
-			if (isGameFinished(board)){
+			if (isGameFinished(board)){ // or if the computer wins, also if there are no valid moves left, the game will end in a tie
 				break;
 			}
 			printBoard(board);
@@ -30,27 +30,14 @@ public class TicTacToe {
 	}
 	
 	private static void printBoard(char[][] board) {
-		System.out.println(board[0][0] + "|" +  board[0][1] + "|" +  board[0][2] );
+		System.out.println(board[0][0] + "|" +  board[0][1] + "|" +  board[0][2] ); // prints the row of the board
 		System.out.println("-+-+-");
 		System.out.println(board[1][0] + "|" +  board[1][1] + "|" +  board[1][2] );
 		System.out.println("-+-+-");
 		System.out.println(board[2][0] + "|" +  board[2][1] + "|" +  board[2][2] );
 	}
 	
-	private static void playerChoice(char[][] board, String position, char symbol) {
-			
-		if(position.equals("1")) board[0][0] = symbol;
-		if(position.equals("2")) board[0][1] = symbol;	
-		if(position.equals("3")) board[0][2] = symbol;	
-		if(position.equals("4")) board[1][0] = symbol;	
-		if(position.equals("5")) board[1][1] = symbol;	
-		if(position.equals("6")) board[1][2] = symbol;	
-		if(position.equals("7")) board[2][0] = symbol;	
-		if(position.equals("8")) board[2][1] = symbol;	
-		if(position.equals("9")) board[2][2] = symbol;	
-	}
-	
-	private static void playerTurn(char[][] board, Scanner scanner) {
+		private static void playerTurn(char[][] board, Scanner scanner) {
 		String userInput;
 		while (true) {
 			System.out.println("Where would you like to play? (1-9)");
@@ -64,8 +51,21 @@ public class TicTacToe {
 		playerChoice(board, userInput, 'X');
 	}
 	
+	private static void playerChoice(char[][] board, String position, char symbol) {
+		// if the player chooses a number from 1 to 9, the element of the board will be marked with an 'X'	
+		if(position.equals("1")) board[0][0] = symbol;
+		if(position.equals("2")) board[0][1] = symbol;	
+		if(position.equals("3")) board[0][2] = symbol;	
+		if(position.equals("4")) board[1][0] = symbol;	
+		if(position.equals("5")) board[1][1] = symbol;	
+		if(position.equals("6")) board[1][2] = symbol;	
+		if(position.equals("7")) board[2][0] = symbol;	
+		if(position.equals("8")) board[2][1] = symbol;	
+		if(position.equals("9")) board[2][2] = symbol;	
+	}
+	
 	private static boolean isValidMove(char[][] board, String position) {
-		
+		// if the element of the board is empty, then it will return true and if none of the elements are empty, it will return false and it will be a tie
 		if(position.equals("1")) return (board[0][0] == ' ');
 		if(position.equals("2")) return (board[0][1] == ' ');
 		if(position.equals("3")) return (board[0][2] == ' ');
@@ -80,7 +80,7 @@ public class TicTacToe {
 	}
 	
 	private static void computerTurn(char[][] board) {
-		Random rand = new Random();
+		Random rand = new Random(); // the computer plays randomly, so it will choose any of the empty elements in the board
 		int computerMove;
 		while (true) {
 			computerMove = rand.nextInt(9) + 1;
@@ -93,16 +93,17 @@ public class TicTacToe {
 	}
 
 	private static boolean hasContestantWon(char[][] board, char symbol) {
+		// if the same row, column or diagonal is marked only with 'X' or '0', then we have a winner
 		if ((board[0][0] == symbol && board [0][1] == symbol && board [0][2] == symbol) ||
-			(board[1][0] == symbol && board [1][1] == symbol && board [1][2] == symbol) ||
-			(board[2][0] == symbol && board [2][1] == symbol && board [2][2] == symbol) ||
+		    (board[1][0] == symbol && board [1][1] == symbol && board [1][2] == symbol) ||
+		    (board[2][0] == symbol && board [2][1] == symbol && board [2][2] == symbol) ||
 			
-			(board[0][0] == symbol && board [1][0] == symbol && board [2][0] == symbol) ||
-			(board[0][1] == symbol && board [1][1] == symbol && board [2][1] == symbol) ||
-			(board[0][2] == symbol && board [1][2] == symbol && board [2][2] == symbol) ||
+		    (board[0][0] == symbol && board [1][0] == symbol && board [2][0] == symbol) ||
+		    (board[0][1] == symbol && board [1][1] == symbol && board [2][1] == symbol) ||
+	       	    (board[0][2] == symbol && board [1][2] == symbol && board [2][2] == symbol) ||
 			
-			(board[0][0] == symbol && board [1][1] == symbol && board [2][2] == symbol) ||
-			(board[0][2] == symbol && board [1][1] == symbol && board [2][0] == symbol) ) {
+		    (board[0][0] == symbol && board [1][1] == symbol && board [2][2] == symbol) ||
+		    (board[0][2] == symbol && board [1][1] == symbol && board [2][0] == symbol) ) {
 			return true;
 		}
 		return false;
@@ -110,13 +111,13 @@ public class TicTacToe {
 	
 	private static boolean isGameFinished(char[][] board) {
 		
-		if (hasContestantWon(board, 'X')) {	
+		if (hasContestantWon(board, 'X')) { // if the function returns true and the criterias which made this function true are met and are equal to 'X', the player wins
 			printBoard(board);
 			System.out.println("You won the game!");
 			return true;
 		}
 		
-		if (hasContestantWon(board, 'O')) {	
+		if (hasContestantWon(board, 'O')) { // the same with the computer, if the function returns true and the met criterias are equal to '0', the computer wins
 			printBoard(board);
 			System.out.println("Computer won the game!");
 			return true;
@@ -126,13 +127,13 @@ public class TicTacToe {
 		while(i<board.length) {
 			int j=0;
 			while(j<board[i].length) {
-				if (board[i][j] == ' ') return false;
+				if (board[i][j] == ' ') return false; // if 'hasContestantWon' returns false and there are still empty elements on the board it will return false, the game continues
 				j++;
 			}
 			i++;
 		}
 		printBoard(board);
-		System.out.println("The game ended in a tie!");
+		System.out.println("The game ended in a tie!"); // if there are no elements left empty on the board and the function 'hasContestantWon' returns false, then it's a tie
 		return true;
 	}
 
