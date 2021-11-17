339bf63761b41db0f328cc5202692f0fed7377ef
Adrian Stefan <92085719+AdrianST19@users.noreply.github.com>
2021-11-17 10:42:18 +0200

    Added a file

--- TicTacToe.java
+++ TicTacToe.java
@@ -38,38 +38,7 @@ public class TicTacToe {
 	}
 	
 	private static void playerChoice(char[][] board, String position, char symbol) {
-//		switch(position) {
-//			case "1":
-//				board[0][0] = symbol;
-//				break;
-//			case "2":
-//				board[0][1] = symbol;
-//				break;
-//			case "3":
-//				board[0][2] = symbol;
-//				break;
-//			case "4":
-//				board[1][0] = symbol;
-//				break;
-//			case "5":
-//				board[1][1] = symbol;
-//				break;
-//			case "6":
-//				board[1][2] = symbol;
-//				break;
-//			case "7":
-//				board[2][0] = symbol;
-//				break;
-//			case "8":
-//				board[2][1] = symbol;
-//				break;
-//			case "9":
-//				board[2][2] = symbol;
-//				break;
-//			default:
-//				System.out.println("...");
-//	}	
-//				
+			
 		if(position.equals("1")) board[0][0] = symbol;
 		if(position.equals("2")) board[0][1] = symbol;	
 		if(position.equals("3")) board[0][2] = symbol;	
@@ -96,28 +65,6 @@ public class TicTacToe {
 	}
 	
 	private static boolean isValidMove(char[][] board, String position) {
-//		switch(position) {
-//			case "1":
-//				return (board[0][0] == ' ');
-//			case "2":
-//				return (board[0][1] == ' ');
-//			case "3":
-//				return (board[0][2] == ' ');
-//			case "4":
-//				return (board[1][0] == ' ');
-//			case "5":
-//				return (board[1][1] == ' ');
-//			case "6":
-//				return (board[1][2] == ' ');
-//			case "7":
-//				return (board[2][0] == ' ');
-//			case "8":
-//				return (board[2][1] == ' ');
-//			case "9":
-//				return (board[2][2] == ' ');
-//			default:
-//				return false;
-//		}
 		
 		if(position.equals("1")) return (board[0][0] == ' ');
 		if(position.equals("2")) return (board[0][1] == ' ');
@@ -175,13 +122,6 @@ public class TicTacToe {
 			return true;
 		}
 		
-//		for (int i = 0; i < board.length; i++) {
-//			for (int j = 0; j < board[i].length; j++) {
-//				if (board[i][j] == ' ') {
-//					return false;
-//				}
-//			}
-//		}
 		int i=0;
 		while(i<board.length) {
 			int j=0;
